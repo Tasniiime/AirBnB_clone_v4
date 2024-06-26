01c7842c03c057aef19016c10d7a42240d360f71
kan-215 <katamialvin369@gmail.com>
2024-06-26 23:43:01 +0300

    AirBnB_clone_v2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,43 +2,43 @@
 
 import json
 
-
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
-    __file_path = 'file.json'
-    __objects = {}
 
-    '''def all(self, cls=None):
-        """Returns a list of objects of one type of class"""
-        if cls is None:
-            return list(FileStorage.__objects.values())
-        else:
-            return [obj for obj in FileStorage.__objects.values() if isinstance(obj, cls)]'''
-    
+    __file_path = 'file.json'  # JSON file path
+    __objects = {}  # Dictionary to store objects
 
     def new(self, obj):
-        """Adds a new object to storage dictionary"""
+        """Adds a new object to the storage dictionary"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
-    
-    def all(self, cls=None):
-        """Returns a dictionary or a filtered dictionary of models currently in storage"""
-        if cls is None:
-            return FileStorage.__objects
-        else:
-            return {key: obj for key, obj in FileStorage.__objects.items() if isinstance(obj, cls)}
 
+    def all(self, cls=None):
+        """Returns a dictionary or a filtered dictionary of models currently in storage."""
+        if cls is None:
+            return self.__objects.copy()  # Return a copy to prevent direct modification
+        else:
+            return {key: obj for key, obj in self.__objects.items() if isinstance(obj, cls)}
+
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it's inside."""
+        if obj is None:
+            return
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        if key in self.__objects:
+            del self.__objects[key]
+            self.save()  # Save changes after deletion
 
     def save(self):
-        """Saves storage dictionary to file"""
+        """Saves the storage dictionary to the JSON file"""
         with open(FileStorage.__file_path, 'w') as f:
             serialized_objects = {}
-            for key, obj in FileStorage.__objects.items():
+            for key, obj in self.__objects.items():
                 serialized_objects[key] = obj.to_dict()
             json.dump(serialized_objects, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """Loads the storage dictionary from the JSON file"""
         try:
             with open(FileStorage.__file_path, 'r') as f:
                 serialized_objects = json.load(f)
@@ -49,14 +49,6 @@ class FileStorage:
         except FileNotFoundError:
             pass
 
-    def delete(self, obj=None):
-        """Deletes obj from __objects if it's inside"""
-        if obj is None:
-            return
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        if key in self.__objects:
-            del self.__objects[key]
-
-    def close():
-        """this method deserialize the json file"""
+    def close(self):
+        """Closes the file storage"""
         self.reload()
