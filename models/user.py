53d6388847866d13793a40a37f81edbb6186d12a
kan-215 <katamialvin369@gmail.com>
2024-06-29 17:46:20 +0300

    AirBnB_clone_v2

--- models/user.py
+++ models/user.py
@@ -1,25 +1,35 @@
-#!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel, Base
-from models import storage_type
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base, storage_type
+from models.city import Place
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+
     __tablename__ = 'users'
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+
     if storage_type == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship('Place', backref='user',
-                              cascade='all, delete, delete-orphan')
-        reviews = relationship('Review', backref='user',
-                               cascade='all, delete, delete-orphan')
+        places = relationship("Place", cascade="all, delete", backref="user")
+        reviews = relationship("Review", cascade="all, delete", backref="user")
     else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+        @property
+        def places(self):
+            """Getter attribute that returns the list of Place instances with user_id equals
+               to the current User.id"""
+            from models import storage
+            place_instances = storage.all(Place)
+            return [place for place in place_instances.values() if place.user_id == self.id]
+
+        @property
+        def reviews(self):
+            """Getter attribute that returns the list of Review instances with user_id equals
+               to the current User.id"""
+            from models import storage
+            review_instances = storage.all(Review)
+            return [review for review in review_instances.values() if review.user_id == self.id]
