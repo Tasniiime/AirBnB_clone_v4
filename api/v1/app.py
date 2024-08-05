8e5f59667816cf99897a34b446f15321b8aec86a
kan-215 <katamialvin369@gmail.com>
2024-08-05 18:26:17 +0300

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,29 +1,48 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
+"""
+Flask application setup
+"""
 
-=======
->>>>>>> f75b135 (initial commit)
-import os
-from flask import Flask
-from models import storage
+from flask import Flask, jsonify
+from flask_cors import CORS
+from os import getenv
+
 from api.v1.views import app_views
+from models import storage
 
-# Create a Flask application instance
+
 app = Flask(__name__)
 
-# Import blueprint app_views and register it to the Flask instance app
+# Enable Cross-Origin Resource Sharing (CORS)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+# Register the app views blueprint
 app.register_blueprint(app_views)
 
-# Define a method to handle teardown_appcontext
+
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """Close the storage on teardown."""
+def teardown(exception):
+    """
+    Teardown context to close the storage
+    """
     storage.close()
 
-if __name__ == "__main__":
-    # Get host and port from environment variables or use default values
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
 
-    # Run the Flask server
-    app.run(host=host, port=port, threaded=True)
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    Handle 404 errors with a JSON response
+    :return: JSON response with error message
+    """
+    data = {
+        "error": "Not found"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 404
+
+    return resp
+
+if __name__ == "__main__":
+    # Run the application using environment variables for host and port
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
