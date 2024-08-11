909d0b27eaef13ff9e6da204451bc68bcf553ad1
kan-215 <katamialvin369@gmail.com>
2024-08-11 22:37:48 +0300

    hbn_v2

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
-"""This script starts the  flask web"""
+
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    """method to display string"""
-    return "Hello HBNB"
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
 
-
-@app.route("/hbnb")
-def display():
-    """method to  display HBNB"""
-    return "HBNB"
-
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""starts a flask web application"""
+
+from flask import Flask, render_template
+from web_flask import *
+app = Flask(__name__)
